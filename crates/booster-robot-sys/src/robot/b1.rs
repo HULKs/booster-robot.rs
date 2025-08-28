cff9920638ab7e7a7d3e3f49f4e0568665006af9
Rasmus Mecklenburg <42248344+rmburg@users.noreply.github.com>
2025-08-28 18:05:29 +0200

    Remove outdated test

--- crates/booster-robot-sys/src/robot/b1.rs
+++ crates/booster-robot-sys/src/robot/b1.rs
@@ -63,26 +63,3 @@ mod ffi {
         fn GetUp(self: Pin<&mut B1LocoClient>) -> i32;
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use cxx::let_cxx_string;
-
-    use crate::robot::{
-        b1::{LocoApiId, b1_loco_client_new},
-        init_channel_factory,
-    };
-
-    #[test]
-    fn api_call() {
-        let_cxx_string!(network_interface = "127.0.0.1");
-        init_channel_factory(&network_interface);
-
-        let api_id = LocoApiId::kWaveHand;
-        let_cxx_string!(param = "");
-        let mut client = b1_loco_client_new();
-
-        client.pin_mut().Init();
-        client.pin_mut().SendApiRequest(api_id, &param);
-    }
-}
