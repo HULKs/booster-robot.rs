ab5dd153969ae22347dd0f7c566395c8868f9a53
Julian Schuler <julianschuler@users.noreply.github.com>
2026-01-09 17:03:25 +0100

    Use manifest path and target arch for linker path

--- crates/booster-robot-sys/build.rs
+++ crates/booster-robot-sys/build.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 fn main() {
     let mut bridge = cxx_build::bridges([
         "src/robot.rs",
@@ -15,10 +17,17 @@ fn main() {
         .include("booster_robotics_sdk/third_party/include")
         .compile("wrapper");
 
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let architecture = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+
     println!("cargo:rerun-if-changed=include/wrapper.hpp");
 
-    println!("cargo::rustc-link-search=native=booster_robotics_sdk/lib/x86_64");
-    println!("cargo::rustc-link-search=native=booster_robotics_sdk/third_party/lib/x86_64");
+    println!(
+        "cargo::rustc-link-search=native={manifest_dir}/booster_robotics_sdk/lib/{architecture}"
+    );
+    println!(
+        "cargo::rustc-link-search=native={manifest_dir}/booster_robotics_sdk/third_party/lib/{architecture}"
+    );
     println!("cargo::rustc-link-lib=static=booster_robotics_sdk");
     println!("cargo::rustc-link-lib=static=foonathan_memory-0.7.3");
     println!("cargo::rustc-link-lib=fastcdr");
